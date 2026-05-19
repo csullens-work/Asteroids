2e61512577d7352c8208e19452dc963ea3f7ec3a
Chris Sullens <csullens@progress.com>
2026-05-19 14:27:12 -0400

    Implement shooting mechanics and collision detection; add Shot class and update player controls

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 """Main entry point for the Asteroids game."""
+import sys
 import pygame
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -17,8 +19,9 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-    Player.containers = (updatable, 
+    Player.containers = (updatable,
                          drawable)
 
     Asteroid.containers = (asteroids,
@@ -26,6 +29,11 @@ def main():
                            drawable)
 
     AsteroidField.containers = (updatable)
+
+    Shot.containers = (shots,
+                        updatable,
+                        drawable)
+
 
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     asteroid_field = AsteroidField()
@@ -50,6 +58,12 @@ def main():
 
         for d in drawable:
             d.draw(screen)
+
+        for a in asteroids:
+            if player.collides_with(a):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         pygame.display.flip()
 
